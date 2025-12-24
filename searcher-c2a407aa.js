e98712691cc4f3c1f668c46802e7f05aaf5a1b3d
JohnSL <JohnSL@users.noreply.github.com>
2025-12-24 20:00:49 +0000

    deploy: f94b885c7270c921d8d55af98ba05bf201803d47

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cf754190.js',
+                path_to_root + 'searchindex-bda53a12.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
